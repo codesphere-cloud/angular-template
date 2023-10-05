c46dfc14184b7290f2aab09076ab18ecb0bed62c
Datata1 <wija1025@h-ka.de>
2023-10-05 19:22:20 +0200

    Code reviewed by Roman

--- angu/src/app/pdf-upload/pdf-upload.component.ts
+++ angu/src/app/pdf-upload/pdf-upload.component.ts
@@ -16,11 +16,9 @@ export class AppPdfUploadComponent {
   // In Ihrer Angular-Komponente (z. B. app-pdf-upload.component.ts)
   newFileName: string = 'neuer_dateiname.pdf'; // Standardwert für den Dateinamen
 
-  async mergePDFs() {
+  async coordinateMergePDFs() {
     const pdfs = await Promise.all(this.uploadedPdfs.map(file => this.readPDF(file)));
-
-    const mergedPdf = await this.combinePDFs(pdfs);
-    this.mergedPdf = mergedPdf;
+    this.mergedPdf = await this.mergePDFsIntoOne(pdfs);
   }
 
 
@@ -28,7 +26,7 @@ export class AppPdfUploadComponent {
     return new Promise<Uint8Array>((resolve, reject) => {
       const reader = new FileReader();
       reader.onload = (event) => {
-        if (event.target && event.target.result) {
+        if (event.target?.result) {
           resolve(new Uint8Array(event.target.result as ArrayBuffer));
         } else {
           reject('Could not read the PDF file');
@@ -38,7 +36,7 @@ export class AppPdfUploadComponent {
     });
   }
 
-  async combinePDFs(pdfs: Uint8Array[]): Promise<Uint8Array> {
+  async mergePDFsIntoOne(pdfs: Uint8Array[]): Promise<Uint8Array> {
     const mergedPdf = await PDFDocument.create();
     for (const pdfBytes of pdfs) {
       const pdf = await PDFDocument.load(pdfBytes);
@@ -50,34 +48,36 @@ export class AppPdfUploadComponent {
     return await mergedPdf.save();
   }
 
-  onFileChange(event: any) {
+  reorderFiles(event: any) {
     const files: FileList = event.target.files;
     for (let i = 0; i < files.length; i++) {
       this.uploadedPdfs.push(files[i]);
     }
-    this.mergePDFs();
+    this.coordinateMergePDFs();
   }
 
 downloadMergedPDF() {
-  if (this.mergedPdf) {
-    const blob = new Blob([this.mergedPdf], { type: 'application/pdf' });
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement('a');
-    document.body.appendChild(a);
-    a.style.display = 'none';
-    a.href = url;
-
-    // Verwenden Sie den Wert aus dem Textfeld als Dateinamen
-    a.download = this.newFileName; // Hier wird der Dateiname aus dem Textfeld verwendet
-
-    a.click();
-    window.URL.revokeObjectURL(url);
-    document.body.removeChild(a);
+  if (!this.mergedPdf) {
+    return;
   }
-}
+  const blob = new Blob([this.mergedPdf], { type: 'application/pdf' });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement('a');
+  document.body.appendChild(a);
+  a.style.display = 'none';
+  a.href = url;
+
+  // Verwenden Sie den Wert aus dem Textfeld als Dateinamen
+  a.download = this.newFileName; // Hier wird der Dateiname aus dem Textfeld verwendet
+
+  a.click();
+  window.URL.revokeObjectURL(url);
+  document.body.removeChild(a);
+  }
+
    drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.uploadedPdfs, event.previousIndex, event.currentIndex);
-	this.mergePDFs();
+	this.coordinateMergePDFs();
   }
 
 }
